04582a96211980648e93e01f11c1dec94ed25f40
Sonia-SV <sonia.sanzvivas@gmail.com>
2020-08-08 22:06:48 +0200

    Another version for fix background in mobile version

--- docs/precache-manifest.a6d8ad7bb9ff8dc42c436498578cb892.js
+++ docs/precache-manifest.a6d8ad7bb9ff8dc42c436498578cb892.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d43c04dbc94e1ded35dffbd4111e8b8c",
+    "revision": "6545acbbd8d8ef159d8e3e0aca646f73",
     "url": "./index.html"
   },
   {
-    "revision": "dd2396e373bfc19706af",
-    "url": "./static/css/main.71f524e9.chunk.css"
+    "revision": "4965ce59c2a154873faa",
+    "url": "./static/css/main.07fd022c.chunk.css"
   },
   {
     "revision": "0159dae550baf4c2db9c",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.d99831e8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dd2396e373bfc19706af",
+    "revision": "4965ce59c2a154873faa",
     "url": "./static/js/main.ff3278c2.chunk.js"
   },
   {
