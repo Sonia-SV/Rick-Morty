78665aa0e9c50718f0ce21e8f278748e183030a9
Sonia-SV <sonia.sanzvivas@gmail.com>
2020-08-08 22:13:35 +0200

    Comeback to the past version of mobile background. Background-attachment doesn't work on mobile browsers

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a6d8ad7bb9ff8dc42c436498578cb892.js"
+  "./precache-manifest.2926cb72ebb43992f5574216ad837469.js"
 );
 
 self.addEventListener('message', (event) => {
